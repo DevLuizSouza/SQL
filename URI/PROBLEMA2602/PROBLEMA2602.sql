38547bf053faa4cbf4ec8bc6430780629c33a926
DevLuizSouza <inboxluizsouza@gmail.com>
2021-10-13 21:11:54 -0300

    Corrigindo erro de digitação

--- URI/PROBLEMA2602/PROBLEMA2602.sql
+++ URI/PROBLEMA2602/PROBLEMA2602.sql
@@ -2,16 +2,16 @@ CREATE DATABASE uri_2602 DEFAULT CHARACTER SET utf8;
 
 USE uri_2602; 
 
-CREATE TABLE custumers(
-id_custumer INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-name_custumer VARCHAR(100) NOT NULL,
-street_custumer VARCHAR(100) not NULL,
-city_custumer VARCHAR(100) NOT NULL,
-state_custumer CHAR(2) NOT NULL,
-credit_limit_custumer FLOAT not NULL
+CREATE TABLE customers(
+id_customer INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name_customer VARCHAR(100) NOT NULL,
+street_customer VARCHAR(100) not NULL,
+city_customer VARCHAR(100) NOT NULL,
+state_customer CHAR(2) NOT NULL,
+credit_limit_customer FLOAT not NULL
 );
 
-INSERT INTO custumers(name_custumer, street_custumer, city_custumer, state_custumer,credit_limit_custumer)
+INSERT INTO customers(name_customer, street_customer, city_customer, state_customer,credit_limit_customer)
 VALUE 
 ('Pedro Augusto da Rocha','Rua Pedro Carlos Hoffman','Porto Alegre', 'RS', 700.00),
 ('Antonio Carlos Mamel','Av. Pinheiros','Belo Horizonte', 'MG', 3500.50),
@@ -19,5 +19,5 @@ VALUE
 ('Jane Ester','Av. 7 de setembro','Erechim', 'RS', 800.00),
 ('Marcos Antônio dos Santos','Av. Farrapos','Porto Alegre', 'RS', 4250.25);
 
-SELECT * FROM custumers 
-WHERE state_custumer = 'RS'; 
+SELECT * FROM customers 
+WHERE state_customer = 'RS'; 
